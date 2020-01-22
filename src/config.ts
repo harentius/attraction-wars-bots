f1fdffdcbf3ac33a2092d1a72a4b1a5846e4e5df
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2020-01-22 18:01:07 +0200

    update balance

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ const config: {
   ticksBeforeChangeDirection: 500,
   minPlayers: 5,
   maxPlayers: 15,
-  tooBigMultiplier: 1.0 / 60.0,
+  tooBigMultiplier: 1.0 / 65.0,
   borderMarginRadiusMultiplier: 2,
 };
 
